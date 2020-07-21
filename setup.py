2d7d9c24a4256f850f901cdf9e986de7e592ef4c
Henrique Soares Assumpção e Silva <henriquesoares@dcc.ufmg.br>
2020-07-21 10:25:26 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="basicMLpy", 
-    version="0.0.6",
+    version="0.0.7",
     author="Henrique Silva",
     author_email="henriquesoares@dcc.ufmg.br",
     description="A collection of simple machine learning algorithms",
