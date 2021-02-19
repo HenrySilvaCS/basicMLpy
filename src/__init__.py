4c88431759b5c32300421d96b803dcf02e9ce910
Henrique Silva <henriquesoares@dcc.ufmg.br>
2021-02-19 12:49:29 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,6 +9,16 @@ Provides
     3. A Cross-Validation algorithm for the functions presented by the package
 =================
 
+Modules
+    1.basicMLpy.regression
+    2.basicMLpy.classification
+    3.basicMLpy.nearest_neighbors
+    4.basicMLpy.ensemble
+    5.basicMLpy.model_selection
+    6.basicMLpy.loss_functions
+    7.basicMLpy.utils
+=================
+
 
 For more information on the package, please visit my Github page or the package page at pypi
 Thanks for using the package and feel free to contact me for suggestions! 
